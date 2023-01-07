7dd109939bc2cca4573215af90ae51faeef0a77e
Mohamed Agnouch <115672435+mohamedagnouch@users.noreply.github.com>
2023-01-07 20:17:23 +0000

    Update

--- src/Components/Poste.jsx
+++ src/Components/Poste.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { addpost ,deletePost} from '../Redux/postsSlice'
+import { addpost ,deletePost,updatePost} from '../Redux/postsSlice'
 
 export default function Poste(e) {
     const [title,setTilte]=useState("")
     const [desc,setDesc]=useState("")
+
+      const [updatetitle, setUpdateTilte] = useState("");
+      const [updatedesc, setUpdateDesc] = useState("");
+
+    const [edit,setEdit]=useState(false)
+    const [id ,setId]=useState(null)
     const postes = useSelector((state) => state.poste.items);
     const dispatch =useDispatch()
   return (
@@ -28,25 +34,56 @@ export default function Poste(e) {
         <button
           onClick={() => {
             dispatch(addpost({ id: postes.length + 1, title, desc }));
-            setTilte("")
-            setDesc("")
+            setTilte("");
+            setDesc("");
           }}
         >
           Add
         </button>
       </div>
       <div className="Posts">
-         {postes.length > 0 ?
-          postes.map(post => (
+        {postes.length > 0
+          ? postes.map((post) => (
               <div key={post.id} className="Post">
-              <p>{post.title}</p>
-              <p>{post.desc}</p>
-              <button>Edit</button>
-              <button onClick={()=>dispatch(deletePost({id:post.id}))}>Delete</button>
+                <p>{post.title}</p>
+                <p>{post.desc}</p>
+                <button
+                  onClick={() => {
+                    setEdit(true);
+                    setId(post.id);
+                  }}
+                >
+                  Edit
+                </button>
+                <br />
+                <button onClick={() => dispatch(deletePost({ id: post.id }))}>
+                  Delete
+                </button>
+                <br />
+                {edit && id === post.id && (
+                  <>
+                    <input
+                      type="text"
+                      placeholder="Update title"
+                      onChange={(e) => setUpdateTilte(e.target.value)}
+                    />
+                    <br />
+                    <input
+                      type="text"
+                      placeholder="Update desc"
+                      onChange={(e) => setUpdateDesc(e.target.value)}
+                    />
+                    <br />
+                    <button onClick={()=>{
+                      dispatch(updatePost({id:post.id,title:updatetitle,desc:updatedesc}));
+                      setEdit(false)
+                    }}>Update</button>
+                  </>
+                )}
               </div>
-              )):"this is not Posts"}
+            ))
+          : "this is not Posts"}
       </div>
-      
     </div>
   );
 }
--- src/Redux/postsSlice.jsx
+++ src/Redux/postsSlice.jsx
@@ -12,8 +12,16 @@ export const postsSlice = createSlice({
     },
     deletePost:function(state,action){
         state.items =state.items.filter(item=>item.id !== action.payload.id) 
+    },
+    updatePost:function(state,action){
+      state.items.map((item) => {
+        if(item.id===action.payload.id){
+          item.title = action.payload.title;
+          item.desc = action.payload.desc;
+        }
+      })
     }
   },
 });
-export const {addpost,deletePost} = postsSlice.actions
+export const { addpost, deletePost, updatePost } = postsSlice.actions;
 export default postsSlice.reducer
